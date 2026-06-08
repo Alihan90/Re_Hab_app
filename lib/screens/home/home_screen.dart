2f125f51e5f64dd0b88afae14224c76f6aa400f0
Alihan90 <137628088+Alihan90@users.noreply.github.com>
2026-06-09 01:26:19 +0300

    Update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -71,7 +71,7 @@ class _HomeScreenState extends State<HomeScreen> {
       const ExercisesTab(),
 
       // ВКЛАДКА 4: НАЛАШТУВАННЯ (Плейсхолдер)
-      const _SettingsTabPlaceholder(),
+      const SettingsTab(),
     ];
 
     final List<String> _titles = [
